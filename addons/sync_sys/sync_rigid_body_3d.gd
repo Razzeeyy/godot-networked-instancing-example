f593841c81072eff9a3504c30c7c380427e46a56
Alexey Shmakov <razzeeyy@gmail.com>
2019-10-14 20:52:02 +0300

    make sure to wake up remote rigid bodies

--- addons/sync_sys/sync_rigid_body_3d.gd
+++ addons/sync_sys/sync_rigid_body_3d.gd
@@ -3,6 +3,8 @@ extends "sync_node.gd"
 export(bool) var interpolate = false
 export(float) var lerp_speed = 10
 
+var dirty = false
+
 
 func _enter_tree():
 	if !node is RigidBody:
@@ -10,6 +12,8 @@ func _enter_tree():
 		return
 	if !is_connected("spawned", self, "_spawned"):
 		connect("spawned", self, "_spawned")
+	if !is_connected("replicated", self, "_replicated"):
+		connect("replicated", self, "_replicated")
 
 
 func _before_spawn():
@@ -21,12 +25,23 @@ func _spawned(data):
 	node.transform = data.transform
 
 
+func _replicated(data):
+	if is_network_master():
+		dirty = false
+	else:
+		dirty = true
+
+
 func integrate_forces(state):
 	if is_network_master():
 		data.transform = state.transform
 		data.linear_velocity = state.linear_velocity
 		data.angular_velocity = state.angular_velocity
 		return
+	
+	if !dirty:
+		return
+	dirty = false
 	
 	if data.has("linear_velocity"):
 		state.linear_velocity = data.linear_velocity
@@ -39,3 +54,8 @@ func integrate_forces(state):
 		state.transform = state.transform.interpolate_with(data.transform, lerp_speed * state.step)
 	else:
 		state.transform = data.transform
+
+
+func _physics_process(delta):
+	if dirty && node.sleeping:
+		node.sleeping = false
